d3f8581058ce1f2bf4c68bc576749948ad03aab3
pragati411 <pragatishukla411@gmail.com>
2023-02-14 16:48:48 +0530

    committed

--- script.js
+++ script.js
@@ -2,14 +2,14 @@
 let card = document.getElementsByClassName("card");
 let cards = [...card];
 
-// deck of all cards in game
+
 const deck = document.getElementById("card-deck");
 
-// declaring move variable
+
 let moves = 0;
 let counter = document.querySelector(".moves");
 
-// declare variables for star icons
+
 const stars = document.querySelectorAll(".fa-star");
 
 // declaring variable of matchedCards
